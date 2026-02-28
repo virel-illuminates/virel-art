193836fa55ccca4df887fd6d7f7b16db56386e64
Virel <virel.illuminates@gmail.com>
2026-02-27 22:37:10 -0500

    fix deploy.sh to include subdirectories like workshop/

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,11 @@ DEST="/var/www/html"
 
 echo "Deploying virel-art to $DEST..."
 cp *.html "$DEST/"
+# Copy subdirectories (workshop, etc.)
+for dir in */; do
+  if [ -d "$dir" ]; then
+    cp -r "$dir" "$DEST/$dir"
+    echo "Copied $dir"
+  fi
+done
 echo "Done."
